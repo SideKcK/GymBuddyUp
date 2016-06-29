59367a589abd33e2b6f42e0ee0b559c593fd103f
you wu <urianawu@tamu.edu>
2016-06-28 22:58:51 -0400

    closes #7

---
+++ GymBuddyUp/VCs/CreateMainVC.swift
@@ -0,0 +1,38 @@
+//
+//  CreateMainVC.swift
+//  GymBuddyUp
+//
+//  Created by you wu on 6/28/16.
+//  Copyright © 2016 You Wu. All rights reserved.
+//
+
+import UIKit
+
+class CreateMainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCancelButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

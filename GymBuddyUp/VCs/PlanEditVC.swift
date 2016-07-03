26b2262441e47289dd1c320317ba05034394af4d
you wu <urianawu@tamu.edu>
2016-07-03 00:07:39 -0400

    closes #19

--- GymBuddyUp/VCs/PlanEditVC.swift
+++ GymBuddyUp/VCs/PlanEditVC.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class PlanEditVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var showDateVisible = false
+    var showRepeatVisible = false
 
+    var cells = ["titleCell", "dateCell", "repeatCell", "detailCell"]
+    var cellNum = 11
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44.0
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
 
         // Do any additional setup after loading the view.
     }
@@ -32,4 +43,55 @@ class PlanEditVC: UIViewController {
     }
     */
 
+}
+extension PlanEditVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellNum
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        if indexPath.row < cellNum - 7 {
+            let cell = tableView.dequeueReusableCellWithIdentifier(cells[indexPath.row], forIndexPath: indexPath)
+            return cell
+        }else if indexPath.row < cellNum - 1 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("exerciseCell", forIndexPath: indexPath)
+            return cell
+        }else {
+            let addCell = tableView.dequeueReusableCellWithIdentifier("addCell", forIndexPath: indexPath)
+            return addCell
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.row == 1 {
+            showDateVisible = !showDateVisible
+            if !showDateVisible {
+                cells.removeAtIndex(2)
+            cellNum -= 1
+            tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: 2, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
+            }else {
+                cellNum += 1
+                cells.insert("datePickerCell", atIndex: 2)
+                tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 2, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
+            }
+
+        }
+        if indexPath.row == 2 {
+            showRepeatVisible = !showRepeatVisible
+            if !showRepeatVisible {
+                cellNum -= 1
+                cells.removeAtIndex(3)
+                tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: 3, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
+            }else {
+                cellNum += 1
+                cells.insert("repeatWeeklyCell", atIndex: 3)
+                tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 3, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
+            }
+
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
 }

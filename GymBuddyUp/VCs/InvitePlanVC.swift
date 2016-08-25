5fd9c837707ebf81efb3f57857365fed38f10311
you wu <urianawu@tamu.edu>
2016-08-25 00:47:16 -0400

    [Invite] in progress

--- GymBuddyUp/VCs/InvitePlanVC.swift
+++ GymBuddyUp/VCs/InvitePlanVC.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class InvitePlanVC: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var tableView: UITableView!
 
+    var plans = [Plan()]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         setupVisual()
+        setupDatepicker()
+        setupTableView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,8 +28,22 @@ class InvitePlanVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupTableView() {
+        tableView.registerNib(UINib(nibName: "WorkoutCell", bundle: nil), forCellReuseIdentifier: "WorkoutCell")
+        tableView.estimatedRowHeight = 120
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func setupDatepicker() {
+        datePicker.minimumDate = NSDate()
+        datePicker.maximumDate = (1.months).fromNow()
+    }
+    
     func setupVisual() {
         self.view.backgroundColor = ColorScheme.s3Bg
+        datePicker.backgroundColor = ColorScheme.s4Bg
         nextButton.backgroundColor = ColorScheme.p1Tint
         nextButton.titleLabel?.textColor = ColorScheme.g4Text
     }
@@ -43,4 +62,27 @@ class InvitePlanVC: UIViewController {
     }
     */
 
+}
+
+extension InvitePlanVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2 + plans.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("noPlanCell", forIndexPath: indexPath)
+            return cell
+        }else if indexPath.row == plans.count + 1 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("addPlanCell", forIndexPath: indexPath)
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("WorkoutCell", forIndexPath: indexPath)
+            return cell
+        }
+    }
+
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.clearColor()
+    }
 }
---
+++ GymBuddyUp/Views/InviteAddPlanCell.swift
@@ -0,0 +1,24 @@
+//
+//  InviteAddPlanCell.swift
+//  GymBuddyUp
+//
+//  Created by you wu on 8/24/16.
+//  Copyright © 2016 You Wu. All rights reserved.
+//
+
+import UIKit
+
+class InviteAddPlanCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

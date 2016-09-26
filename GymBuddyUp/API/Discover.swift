38f1d0f6f5d185ca8b8c0b8662620bfa3784278f
wei <waix@msn.com>
2016-09-26 02:34:54 -0700

    fixed an issue with discover friends

--- GymBuddyUp/API/Discover.swift
+++ GymBuddyUp/API/Discover.swift
@@ -79,30 +79,39 @@ class Discover {
         // 2. for each friend, get active published workout. (workoutTime >= today && type > private (access > 0, 0 is private, 1 is friends, 2 is public) )
         // 3. for each workout id, filter by available = true
         // 4. for each workout 
-//        var invites = [Invite]()
-//        let getInvitesTaskGrp = dispatch_group_create()
-//        
-//        userFriendsRef.child((User.currentUser?.userId)!).queryOrderedByChild("is_friend").queryEqualToValue(1).observeSingleEventOfType(.Value, withBlock: {(snapshot) in
-//            for friend in snapshot.children {
-//                // get this friend's active workout
-//                let friendUid = (friend as! FIRDataSnapshot).key
-//          
-//                dispatch_group_enter(getInvitesTaskGrp)
-//                
-//                userWorkoutInviteRef.child(friendUid).queryOrderedByChild(String).query
-//                getPlanById(planId, completion: { (plan, error) in
-//                    if let plan = plan {
-//                        print("getting plan", planId)
-//                        plans.append(plan)
-//                        dispatch_group_leave(getPlanTaskGrp)
-//                    }
-//                })
-//            }
-//            
-//            dispatch_group_notify(getInvitesTaskGrp, dispatch_get_main_queue()) {
-//                completion(plans: plans, error: nil)
-//            }
-//        })
+        var invites = [Invite]()
+        let getInvitesTaskGrp = dispatch_group_create()
+        
+        userFriendsRef.child((User.currentUser?.userId)!).queryOrderedByChild("is_friend").queryEqualToValue(1).observeSingleEventOfType(.Value, withBlock: {(snapshot) in
+            for friend in snapshot.children {
+                // get this friend's active workout
+                let friendUid = (friend as! FIRDataSnapshot).key
+          
+                dispatch_group_enter(getInvitesTaskGrp)
+                
+                userWorkoutInviteRef.child(friendUid).queryOrderedByChild("workout_time").queryStartingAtValue(NSDate().timeIntervalSince1970).observeSingleEventOfType(.Value, withBlock: { (userWorkoutInviteRef) in
+                    let invite = userWorkoutInviteRef.valueForKey("invite") as! String
+                    
+                    // now get this invite object
+                    workoutInviteRef.child(invite).queryOrderedByChild("access").queryStartingAtValue(1).observeSingleEventOfType(.Value, withBlock: { (inviteSnapshot) in
+                        let inviteData = inviteSnapshot.value as! [String: AnyObject]
+                        if let invite = Invite(JSON: inviteData){
+                            if invite.isAvailable != false {
+                                invites.append(invite)
+                            }
+                        }
+                        
+                        dispatch_group_leave(getInvitesTaskGrp)
+                    })
+                })
+            }
+            
+            dispatch_group_notify(getInvitesTaskGrp, dispatch_get_main_queue()) {
+                completion(invites, nil)
+            }
+        })
     }
+    
+    
 
 }
